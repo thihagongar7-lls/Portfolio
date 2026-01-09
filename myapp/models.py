b0b056426b920ecf4ea9e878d054a87919ffd07b
Myat ThiHa Tun <thihagongar7@example.com>
2026-01-09 20:13:06 +0630

    'third'

--- myapp/models.py
+++ myapp/models.py
@@ -102,3 +102,166 @@ class ServiceEnd(models.Model):
     image = models.ImageField(upload_to='service_end/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
+
+class Portfolio(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    tagline = RichTextField()
+    title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)     
+
+class Card(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    pri_image = models.ImageField(upload_to='card/')
+    sec_image = models.ImageField(upload_to='card/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class Content(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    thm_pri_text = RichTextField()
+    thm_sec_text = RichTextField()
+    thm_btn= models.URLField(null=True,blank=True)
+    thm_text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)   
+
+class SecondCard(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    pri_image = models.ImageField(upload_to='sec_card/')
+    sec_image = models.ImageField(upload_to='sec_card/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class SecondContent(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    thm_pri_text = RichTextField()
+    thm_sec_text = RichTextField()
+    thm_third_text = RichTextField()
+    thm_btn= models.URLField(null=True,blank=True)
+    thm_text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class ThirdCard(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    pri_image = models.ImageField(upload_to='third_card/')
+    sec_image = models.ImageField(upload_to='third_card/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class ThirdContent(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    thm_pri_text = RichTextField()
+    thm_sec_text = RichTextField()
+    thm_third_text = RichTextField()
+    thm_btn= models.URLField(null=True,blank=True)
+    thm_text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class FourthCard(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    pri_image = models.ImageField(upload_to='fourth_card/')
+    sec_image = models.ImageField(upload_to='fourth_card/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class FourthContent(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    thm_pri_text = RichTextField()
+    thm_sec_text = RichTextField()
+    thm_btn= models.URLField(null=True,blank=True)
+    thm_text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+    
+class PortfolioEnd(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    thm_btn= models.URLField(null=True,blank=True)
+    thm_text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class Experience(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    tagline = RichTextField()
+    title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)     
+
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    image = models.ImageField(upload_to='education/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)   
+
+class Trainer(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+
+class Assistant(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+
+class Design(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+
+class MinDesign(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
+
+class Custom(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title = RichTextField()
+    image = models.ImageField(upload_to='custom/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class MainContent(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    tagline = RichTextField()
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+class MinContent(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    tagline = RichTextField()
+    title = RichTextField()
+    min_title = RichTextField()
+    description = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+class ExperiencesImage(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    image = models.ImageField(upload_to='experiences_image/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)     
--- myapp/views.py
+++ myapp/views.py
@@ -6,18 +6,43 @@ from .models import *
 def Homepage(request):
     banner = BannerModel.objects.all()
     text = TextModel.objects.all()
+
     main_about = MinAbout.objects.all()
     me_about = MeAbout.objects.all()
     about =About.objects.all()
     min_about = MinAbout.objects.all()
     max_about =MaxAbout.objects.all()
     end_about =EndAbout.objects.all()
+
     section_header =SectionHeader.objects.all()
+
     services = Services.objects.all()
     service_one = ServiceOne.objects.all()
     service_two = ServiceTwo.objects.all()
     service_three =ServiceThree.objects.all()
     service_end = ServiceEnd.objects.all()
+
+    portfolio = Portfolio.objects.all()
+    card = Card.objects.all()
+    content = Content.objects.all()
+    second_card = SecondCard.objects.all()
+    second_content = SecondContent.objects.all()
+    third_card = ThirdCard.objects.all()
+    third_content = ThirdContent.objects.all()
+    fourth_card = FourthCard.objects.all()
+    fourth_content = FourthContent.objects.all()
+    portfolio_end = PortfolioEnd.objects.all()
+    
+    experience = Experience.objects.all()
+    education =Education.objects.all()
+    trainer = Trainer.objects.all()
+    assistant =Assistant.objects.all()
+    design = Design.objects.all()
+    min_design = MinDesign.objects.all()
+    custom = Custom.objects.all()
+    main_content =MainContent.objects.all()
+    min_content =MinContent.objects.all()
+    experiences_image = ExperiencesImage.objects.all()
     contex = {
         'banner':banner,
         'text':text,
@@ -32,6 +57,26 @@ def Homepage(request):
         'service_one':service_one,
         'service_two':service_two,
         'service_three':service_three,
-        'service_end':service_end
+        'service_end':service_end,
+        'portfolio': portfolio,
+        'card':card,
+        'content': content,
+        'second_card':second_card,
+        'second_content':second_content,
+        'third_card':third_card,
+        'third_content':third_content,
+        'fourth_card':fourth_card,
+        'fourth_content':fourth_content,
+        'portfolio_end':portfolio_end,
+        'experience':experience,
+        'education':education,
+        'trainer':trainer,
+        'assistant':assistant,
+        'design':design,
+        'min_design':min_design,
+        'custom':custom,
+        'main_content':main_content,
+        'min_content':min_content,
+        'experiences_image':experiences_image
     }
     return render(request, 'index.html', contex)
